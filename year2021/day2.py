beb549cf91b8c8d22a637c645f4fb60296d2b85e
JKrecisz <jakub.krecisz@icloud.com>
2022-02-23 16:47:11 +0100

    Day 2 completed

--- year2021/day2.py
+++ year2021/day2.py
@@ -14,6 +14,20 @@ def multiply(myList):
     return horizontalPos * depth
 
 
+def secondMultiply(myList):
+    horizontalPos, depth, aim = 0, 0, 0
+    for el in myList:
+        toListEl = el.split()
+        if toListEl[0] == 'forward':
+            horizontalPos += int(toListEl[1])
+            depth += int(toListEl[1])*aim
+        elif toListEl[0] == 'down':
+            aim += int(toListEl[1])
+        else:
+            aim -= int(toListEl[1])
+    return horizontalPos * depth
+
+
 """ ---- PART ONE ---- """
 
 try:
@@ -22,3 +36,7 @@ try:
         myList.append(input())
 except:
     print(f'The result of multiplying horizontal position by depth is: {multiply(myList)}')
+
+""" ---- PART TWO ---- """
+
+print(f'The second result of multiplying horizontal position by depth is: {secondMultiply(myList)}')
