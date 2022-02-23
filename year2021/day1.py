86872dd0431a3b2bf71f3ee2065cfe8977819cfc
JKrecisz <jakub.krecisz@icloud.com>
2022-02-23 15:49:44 +0100

    Day 1 completed

--- year2021/day1.py
+++ year2021/day1.py
@@ -8,6 +8,13 @@ def measurement(listOfNumbers):
             countOfIncrease += 1
     return countOfIncrease
 
+def secondMeasurement(listOfNumbers):
+    countOfIncrease = 0
+    for index in range(1, len(listOfNumbers) - 2):
+        if (listOfNumbers[index - 1] + listOfNumbers[index] + listOfNumbers[index + 1]) <\
+                (listOfNumbers[index] + listOfNumbers[index + 1] + listOfNumbers[index + 2]):
+            countOfIncrease += 1
+    return countOfIncrease
 
 try:
     myList = []
@@ -15,3 +22,9 @@ try:
         myList.append(int(input()))
 except:
     print(f'Measurements which are larger than the previous one: {measurement(myList)}')
+
+
+""" ---- PART TWO ---- """
+
+print(f'Three measurements which are larger than the three previous measurements: {secondMeasurement(myList)}')
+
