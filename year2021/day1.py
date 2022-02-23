855fe228f64f75793f545e7912f71dae7deaa8f3
JKrecisz <jakub.krecisz@icloud.com>
2022-02-23 15:31:50 +0100

    Day 1 - part one

--- year2021/day1.py
+++ year2021/day1.py
@@ -1,24 +1,17 @@
-listOfNumbers = [199,
-                 200,
-                 208,
-                 210,
-                 200,
-                 207,
-                 240,
-                 269,
-                 260,
-                 263]
+""" ---- PART ONE ---- """
 
-prefix = [' (N/A - no previous measurement)']
 
-for index in range(1, len(listOfNumbers)):
-    if listOfNumbers[index - 1] > listOfNumbers[index]:
-        prefix.append(' (Decreased)')
-    else:
-        prefix.append(' (Increased)')
+def measurement(listOfNumbers):
+    countOfIncrease = 0
+    for index in range(1, len(listOfNumbers)):
+        if listOfNumbers[index - 1] < listOfNumbers[index]:
+            countOfIncrease += 1
+    return countOfIncrease
 
-for num in range(len(listOfNumbers)):
-    listOfNumbers[num] = str(listOfNumbers[num]) + prefix[num]
 
-for el in listOfNumbers:
-    print(el)
+try:
+    myList = []
+    while True:
+        myList.append(int(input()))
+except:
+    print(f'Measurements which are larger than the previous one: {measurement(myList)}')
