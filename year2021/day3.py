5fc33f21c20eb5fd2892c9d17544ec466547d138
JKrecisz <jakub.krecisz@icloud.com>
2022-02-24 18:33:28 +0100

    Day 3

--- year2021/day3.py
+++ year2021/day3.py
@@ -1,9 +1,26 @@
 """ --- Day 3: Binary Diagnostic --- """
 
 
+def getGammaRate(myList):
+    gamma = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for element in myList:
+        for i in range(len(myList[0])):
+            if element[i] == '0':
+                gamma[i] -= 1
+            else:
+                gamma[i] += 1
+
+    for index in range(len(gamma)):
+        if gamma[index] <= 0:
+            gamma[index] = 0
+        else:
+            gamma[index] = 1
+    return ''.join(str(gamma))
+
+
 try:
     myList = []
     while True:
-        myList.append(int(input()))
+        myList.append(input())
 except:
-    print(myList)
+    print(getGammaRate(myList))
