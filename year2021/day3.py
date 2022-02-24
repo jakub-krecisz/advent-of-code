c5a9b759bd04eb2b5fa9839aea3f7b4716f5c121
JKrecisz <jakub.krecisz@icloud.com>
2022-02-24 23:32:55 +0100

    Day 3 part one

--- year2021/day3.py
+++ year2021/day3.py
@@ -2,7 +2,8 @@
 
 
 def getGammaRate(myList):
-    gamma = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    gamma = [0 for _ in range(len(myList[0]))]
+    decimal = 0
     for element in myList:
         for i in range(len(myList[0])):
             if element[i] == '0':
@@ -15,7 +16,18 @@ def getGammaRate(myList):
             gamma[index] = 0
         else:
             gamma[index] = 1
-    return ''.join(str(gamma))
+    print(gamma)
+    gamma.reverse()
+    for i in range(len(gamma)):
+        decimal = decimal + (2 ** i) * gamma[i]
+    return decimal
+
+
+def getEpsilonRate(list):
+    for i in range(len(list)):
+        list[i] = 0
+        print(list[i])
+    return list
 
 
 try:
@@ -23,4 +35,5 @@ try:
     while True:
         myList.append(input())
 except:
-    print(getGammaRate(myList))
+    gamma = getGammaRate(myList)
+    print('The power consumption is: ' + str(gamma * (2 ** len(myList[0]) - gamma - 1)))
