b956dcd16d8397682247d2680e4282a10123a85e
JKrecisz <jakub.krecisz@icloud.com>
2022-02-25 02:07:47 +0100

    Day 3 part two

--- year2021/day3.py
+++ year2021/day3.py
@@ -16,19 +16,33 @@ def getGammaRate(myList):
             gamma[index] = 0
         else:
             gamma[index] = 1
-    print(gamma)
     gamma.reverse()
     for i in range(len(gamma)):
         decimal = decimal + (2 ** i) * gamma[i]
     return decimal
 
 
-def getEpsilonRate(list):
-    for i in range(len(list)):
-        list[i] = 0
-        print(list[i])
-    return list
+def getOxygenRate(myList):
+    oxygen = [0 for _ in range(len(myList[0]))]
+    decimal = 0
+    for element in myList:
+        for i in range(len(myList[0])):
+            if element[i] == '0':
+                oxygen[i] -= 1
+            else:
+                oxygen[i] += 1
+    print(oxygen)
 
+    for index in range(len(oxygen)):
+        if oxygen[index] >= 0:
+            oxygen[index] = 1
+        else:
+            oxygen[index] = 0
+    print(oxygen)
+    oxygen.reverse()
+    for i in range(len(oxygen)):
+        decimal = decimal + (2 ** i) * oxygen[i]
+    return decimal
 
 try:
     myList = []
@@ -37,3 +51,4 @@ try:
 except:
     gamma = getGammaRate(myList)
     print('The power consumption is: ' + str(gamma * (2 ** len(myList[0]) - gamma - 1)))
+    print('Oxygen generator rate is: ' + str(getOxygenRate(myList)))
