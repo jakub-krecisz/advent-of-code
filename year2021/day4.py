5feff2bdca0247a954ae2d52c67694a6da68ae66
JKrecisz <jakub.krecisz@icloud.com>
2022-02-25 02:41:49 +0100

    Day 4 almost done

--- year2021/day4.py
+++ year2021/day4.py
@@ -4,13 +4,21 @@ def getBingoList(myList):
     correctedList = []
     for i in range(1, len(myList)):
         correctedList.append(myList[i].split())
+    correctedList.remove([])
+    correctedList.remove([])
     return correctedList
 
 def getScore(myList):
     numbers = myList[0].split(',')
     myList.remove('')
     bingoList = getBingoList(myList)
-    print('ok')
+    counter = [0 for _ in range(len(bingoList))]
+    for index in range(len(bingoList)):
+        for num in numbers:
+            if num in bingoList[index]:
+                counter[index] += 1
+                if counter[index] == 5:
+                    return bingoList[index]
 
 
 try:
@@ -18,4 +26,4 @@ try:
     while True:
         myList.append(input())
 except:
-    getScore(myList)
+    print(getScore(myList))
