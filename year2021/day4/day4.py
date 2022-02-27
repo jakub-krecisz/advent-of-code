6dc3fdf7b5b6681c5b1bc08937ebd7cf4fee6e00
JKrecisz <jakub.krecisz@icloud.com>
2022-02-27 23:56:17 +0100

    day 4

--- year2021/day4/day4.py
+++ year2021/day4/day4.py
@@ -1,17 +1,22 @@
 """ --- Day 4: Giant Squid --- """
 
+
 def getBingoList(myList):
     correctedList = []
     for i in range(1, len(myList)):
         correctedList.append(myList[i].split())
-
-    return correctedList
-
+        finalList = []
+        list = []
+    for index in range(len(correctedList)):
+        if index % 6 == 0:
+            finalList.append(list)
+            list.clear()
+        list.append(correctedList[index])
+    return finalList
 
 
 def getResult(wonList, bingoList):
     result = 0
-
     wonList = correctList(wonList)
     bingoList = correctList(bingoList)
     for bingoListEl in bingoList:
@@ -19,18 +24,19 @@ def getResult(wonList, bingoList):
             result += int(bingoListEl)
     return result
 
+
 def correctList(list):
     correctedList = []
     for el in list:
         correctedList += el
     return correctedList
 
+
 def getScore(myList):
     numbers = myList[0].split(',')
     myList.remove('')
     bingoList = getBingoList(myList)
-    print(bingoList)
-    counter = [[] for _ in range(len(bingoList)*5)]
+    counter = [[] for _ in range(len(bingoList) * 5)]
     for groupIndex in range(len(bingoList)):
         for number in numbers:
             for index in range(len(bingoList[groupIndex])):
@@ -38,7 +44,6 @@ def getScore(myList):
                     counter[index].append(number)
                     if len(counter[index]) == 5:
                         return int(getResult(counter, bingoList[groupIndex])) * int(number)
-
 
 
 try:
