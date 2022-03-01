b7961c78ff4c4851bec2683b0201fb6288f33025
JKrecisz <jakub.krecisz@icloud.com>
2022-03-01 23:58:40 +0100

    day 5

--- year2021/day5/day5.py
+++ year2021/day5/day5.py
@@ -1,2 +1,39 @@
+
+def correctList(list):
+    correctedList = []
+    for el in list:
+        coords = el[:el.find(' -> ')].split(',') + el[el.find(' -> ')+4:].split(',')
+        correctedList.append(coords)
+    return correctedList
+
+def getCross(list):
+    x, y = 0, 0
+    for el in list:
+        if x < max(int(el[0]), int(el[2])):
+            x = max(int(el[0]), int(el[2]))
+        if y < max(int(el[1]), int(el[3])):
+            y = max(int(el[1]), int(el[3]))
+    cross = [[0 for _ in range(x)] for _ in range(y)]
+    return cross
+
+
+def getNumOfPoints(myList):
+    myList = correctList(myList)
+    cross = getCross(myList)
+    cross = fillCross(cross, myList)
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    pass
+    try:
+        myList = []
+        while True:
+            myList.append(input())
+    except:
+        print(getNumOfPoints(myList))
