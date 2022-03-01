4af4af254d66871018468a0009446ad501ea8662
JKrecisz <jakub.krecisz@icloud.com>
2022-03-02 00:34:58 +0100

    day 5 Part One

--- year2021/day5/day5.py
+++ year2021/day5/day5.py
@@ -1,19 +1,33 @@
-
 def correctList(list):
     correctedList = []
     for el in list:
-        coords = el[:el.find(' -> ')].split(',') + el[el.find(' -> ')+4:].split(',')
+        coords = el[:el.find(' -> ')].split(',') + el[el.find(' -> ') + 4:].split(',')
         correctedList.append(coords)
+    for groupIndex in range(len(correctedList)):
+        for index in range(len(correctedList[groupIndex])):
+            correctedList[groupIndex][index] = int(correctedList[groupIndex][index])
     return correctedList
+
 
 def getCross(list):
     x, y = 0, 0
     for el in list:
-        if x < max(int(el[0]), int(el[2])):
-            x = max(int(el[0]), int(el[2]))
-        if y < max(int(el[1]), int(el[3])):
-            y = max(int(el[1]), int(el[3]))
-    cross = [[0 for _ in range(x)] for _ in range(y)]
+        if x < max(el[0], el[2]):
+            x = max(el[0], el[2])
+        if y < max(el[1], el[3]):
+            y = max(el[1], el[3])
+    cross = [[0 for _ in range(x + 1)] for _ in range(y + 1)]
+    return cross
+
+
+def fillCross(cross, coords):
+    for coordinate in coords:
+        if coordinate[0] == coordinate[2]:
+            for yIndex in range(min(coordinate[1], coordinate[3]), max(coordinate[1], coordinate[3]) + 1):
+                cross[yIndex][coordinate[0]] += 1
+        elif coordinate[1] == coordinate[3]:
+            for xIndex in range(min(coordinate[0], coordinate[2]), max(coordinate[0], coordinate[2]) + 1):
+                cross[coordinate[1]][xIndex] += 1
     return cross
 
 
@@ -21,13 +35,12 @@ def getNumOfPoints(myList):
     myList = correctList(myList)
     cross = getCross(myList)
     cross = fillCross(cross, myList)
-
-
-
-
-
-
-
+    counter = 0
+    for groupIndex in range(len(cross)):
+        for index in range(len(cross[groupIndex])):
+            if cross[groupIndex][index] >=2 :
+                counter += 1
+    return counter
 
 
 if __name__ == '__main__':
