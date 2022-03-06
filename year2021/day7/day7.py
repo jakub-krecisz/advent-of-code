d39e063a7d639b2e28f24fcb2c357b3fd4ae494e
JKrecisz <jakub.krecisz@icloud.com>
2022-03-06 01:05:16 +0100

    day 7

--- year2021/day7/day7.py
+++ year2021/day7/day7.py
@@ -1,4 +1,30 @@
+""" --- Day 7: The Treachery of Whales --- """
+
+
+def getKey(leasFuel, dictionary):
+    for key, value in dictionary.items():
+        if leasFuel == value:
+            return key
+
+
+def getPosAndFuel(data):
+    maxPosition, minPosition = max(data), min(data)
+    positions = {}
+    for pos in range(minPosition, maxPosition + 1):
+        fuel = 0
+        for crab in data:
+            fuel += abs(pos - crab)
+        positions[pos] = fuel
+    leastFuel = min(positions.values())
+    bestPosition = getKey(leastFuel, positions)
+
+    return f'Best position is: {bestPosition}, this position need {leastFuel} fuel'
+
+
 if __name__ == '__main__':
     dataFile = open('data.txt')
     dataInput = dataFile.read().split(',')
-    print(dataInput)
+    toInt = map(int, dataInput)
+    intDataInput = list(toInt)
+
+    print(getPosAndFuel(intDataInput))
