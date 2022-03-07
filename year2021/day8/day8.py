3627b2173fd1e50175f1d493318338f182782c5e
JKrecisz <jakub.krecisz@icloud.com>
2022-03-07 02:54:35 +0100

    day 8

--- year2021/day8/day8.py
+++ year2021/day8/day8.py
@@ -1,6 +1,18 @@
 """ --- Day 8: Seven Segment Search --- """
 
+
+def getNum(data):
+    data = data.split()
+    uniqueDigits = [2, 3, 4, 7]
+    counter = 0
+    for i in range(11, len(data), 15):
+        for index in range(i, i + 4):
+            if len(data[index]) in uniqueDigits:
+                counter += 1
+    return counter
+
+
 if __name__ == '__main__':
     dataFile = open('data.txt')
-    dataInput = dataFile.read().split(' ')
-    print(dataInput)
+    dataInput = dataFile.read()
+    print(getNum(dataInput))
