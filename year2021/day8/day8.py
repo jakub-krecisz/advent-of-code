825b2b63f10a73ac0ac08db5a188e93df47a3f2b
JKrecisz <jakub.krecisz@icloud.com>
2022-03-11 22:45:26 +0100

    day8

--- year2021/day8/day8.py
+++ year2021/day8/day8.py
@@ -1,11 +1,27 @@
 """ --- Day 8: Seven Segment Search --- """
 
+def getKey(value, dataTuple):
+    for key, val in dataTuple.items():
+        if val == value:
+            return key
 
 def getTopWord(twoLen, threeLen):
     twoLenList, threeLenList = list(twoLen[0]), list(threeLen[0])
     for el in threeLenList:
         if el not in twoLenList:
             return el
+
+
+def getTopLeftWord(twoLen, fourLen, fiveLen):
+    filtered = []
+    for el in list(fourLen):
+        if el not in list(twoLen):
+            filtered.append(el)
+    for el in fiveLen:
+        if filtered[0] not in list(el):
+            return filtered[0]
+        elif filtered[1] not in list(el):
+            return filtered[1]
 
 
 def getRightTopWord(twoLen, sixLen):
@@ -16,6 +32,33 @@ def getRightTopWord(twoLen, sixLen):
         elif twoLenList[1] not in list(el):
             return twoLenList[1]
 
+
+def getCenterWord(twoLen, fourLen, fiveLen):
+    filtered = []
+    for el in list(fourLen):
+        if el not in list(twoLen):
+            filtered.append(el)
+    for el in fiveLen:
+        if filtered[0] not in list(el):
+            return filtered[1]
+        elif filtered[1] not in list(el):
+            return filtered[0]
+
+
+def getLeftBottomWord(display, sixLen, centerWord):
+    filtered = []
+    for el in sixLen:
+        if centerWord not in list(el):
+            for word in list(el):
+                if word not in display.values():
+                    filtered.append(word)
+    for el in sixLen:
+        if filtered[0] not in list(el):
+            return filtered[0]
+        elif filtered[1] not in list(el):
+            return filtered[1]
+
+
 def getRightBottomWord(twoLen, sixLen):
     twoLenList = list(twoLen[0])
     for el in sixLen:
@@ -23,6 +66,39 @@ def getRightBottomWord(twoLen, sixLen):
             return twoLenList[1]
         elif twoLenList[1] not in list(el):
             return twoLenList[0]
+
+
+def getBottomWord(display, sixLen, centerWord):
+    filtered = []
+    for el in sixLen:
+        if centerWord not in list(el):
+            for word in list(el):
+                if word not in display.values():
+                    filtered.append(word)
+    for el in sixLen:
+        for word in list(el):
+            if word not in display.values():
+                return word
+
+
+def getValueFromStr(value, display):
+    numbers = {
+        1: [display[2], display[5]],
+        2: [display[0], display[2], display[3], display[4], display[6]],
+        3: [display[0], display[2], display[3], display[5], display[6]],
+        4: [display[1], display[2], display[3], display[5]],
+        5: [display[0], display[1], display[3], display[5], display[6]],
+        6: [display[0], display[1], display[3], display[4], display[5], display[6]],
+        7: [display[0], display[2], display[5]],
+        8: [display[i] for i in range(7)],
+        9: [display[0], display[1], display[2], display[3], display[5], display[6]],
+        0: [display[0], display[1], display[2], display[4], display[5], display[6]]
+    }
+
+    for val in numbers.values():
+        if all(word in val for word in list(value)):
+            if all(word in list(value) for word in val):
+                return getKey(val, numbers)
 
 
 def getValue(data):
@@ -35,16 +111,21 @@ def getValue(data):
         7: [],
     }
     display = {}
+    summary = 0
     for index in range(10):
         for length in range(2, 8):
             if int(len(data[index])) == length:
                 numLen[length].append(data[index])
     display[0] = getTopWord(numLen[2], numLen[3])
+    display[1] = getTopLeftWord(numLen[2][0], numLen[4][0], numLen[5])
     display[2] = getRightTopWord(numLen[2], numLen[6])
+    display[3] = getCenterWord(numLen[2][0], numLen[4][0], numLen[5])
     display[5] = getRightBottomWord(numLen[2], numLen[6])
-    print(display)
-    print(numLen)
-    return 0
+    display[4] = getLeftBottomWord(display, numLen[6], display[3])
+    display[6] = getBottomWord(display, numLen[6], display[3])
+    for index in range(11, 15):
+        summary += getValueFromStr(data[index], display)
+    return summary
 
 
 def separateData(data):
@@ -62,7 +143,8 @@ def getSum(data):
     total = 0
     for el in data:
         total += getValue(el)
-    return data
+        print(getValue(el))
+    return total
 
 
 def getNum(data):
@@ -79,5 +161,5 @@ def getNum(data):
 if __name__ == '__main__':
     dataFile = open('data.txt')
     dataInput = dataFile.read()
-    print(getNum(dataInput))
+    print(f'How many times numbers appear: {getNum(dataInput)}')
     print(getSum(dataInput))
