11c4a77afe0da0e3f161d323e5b1ec7d32e28aaa
JKrecisz <jakub.krecisz@icloud.com>
2022-03-22 00:59:38 +0100

    day 12

--- year2021/day12/day12.py
+++ year2021/day12/day12.py
@@ -8,6 +8,7 @@ def getData(fileName):
         dataIn.append((p1, p2))
     return dataIn
 
+
 def traverse(cave):
     global current_path
     for x in cave:
@@ -23,19 +24,56 @@ def traverse(cave):
     current_path.pop()
 
 
-if __name__ == '__main__':
-    dataIn = getData('data.txt')
-    M = {}
-    path = []
+def secondTraverse(cave):
+    global current_path
+    global second_small
+    for x in cave:
+        if x.islower() and x in current_path:
+            if not second_small[0] and x != "start" and x != "end":
+                second_small = (True, x)
+                current_path.append(x)
+                secondTraverse(M[x])
+            continue
+        elif x == 'end':
+            current_path.append(x)
+            path.append(current_path[:])
+            current_path.pop()
+        else:
+            current_path.append(x)
+            secondTraverse(M[x])
+    if current_path.pop() == second_small[1]:
+        second_small = (False, "")
 
-    for row in dataIn:
+
+if __name__ == '__main__':
+    # Part one
+    M, path = {}, []
+    for row in getData('data.txt'):
         if row[0] not in M:
             M[row[0]] = []
         M[row[0]].append(row[1])
         if row[1] not in M:
             M[row[1]] = []
         M[row[1]].append(row[0])
+
     current_path = ["start"]
+    second_small = (False, "")
     traverse(M["start"])
 
     print(f'Paths that visit small caves at most once is: {len(path)}')
+
+    # Part Two
+    M, path = {}, []
+    for a in getData('data.txt'):
+        if a[0] not in M:
+            M[a[0]] = []
+        M[a[0]].append(a[1])
+        if a[1] not in M:
+            M[a[1]] = []
+        M[a[1]].append(a[0])
+
+    current_path = ["start"]
+    second_small = (False, "")
+    secondTraverse(M["start"])
+
+    print(f'Paths through this cave system is: {len(path)}')
