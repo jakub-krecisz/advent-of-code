6795961dec96ab87be961546a0a93eae82e5c96c
JKrecisz <jakub.krecisz@icloud.com>
2022-04-10 21:11:43 +0200

    day 14

--- year2021/day14/day14.py
+++ year2021/day14/day14.py
@@ -10,13 +10,64 @@ def getInsertions(dataInput):
 def getWord(first, second, pairInsertions):
     return pairInsertions[first + second]
 
+
 def getMostCommon(polymerTemplate):
     words = set(polymerTemplate)
     return max(polymerTemplate.count(word) for word in words)
 
+
 def getLeastCommon(polymerTemplate):
     words = set(polymerTemplate)
     return min(polymerTemplate.count(word) for word in words)
+
+def getPairs(pairInsertions):
+    pairs = {}
+    for pair in pairInsertions.keys():
+        pairs[pair] = 0
+    return pairs
+
+def getPairIntersection(pairs, pairInsertions):
+    newPairs = []
+    for pair in pairs.keys():
+        newPairs.append(pair[0] + pairInsertions[pair])
+        newPairs.append(pairInsertions[pair] + pair[1])
+    pairs.clear()
+
+    for el in newPairs:
+        if el not in pairs:
+            pairs[el] = 1
+        else:
+            pairs[el] += 1
+
+    return pairs
+
+def getSecondResult(dataInput, steps):
+    polymerTemplate = dataInput[0]
+    pairInsertions = getInsertions(dataInput)
+    lastWord = polymerTemplate[-1]
+    pairs = {}
+    for index in range(1, len(polymerTemplate)):
+        pairs[polymerTemplate[index - 1] + polymerTemplate[index]] = 0
+    for index in range(1, len(polymerTemplate)):
+        pairs[polymerTemplate[index - 1] + polymerTemplate[index]] += 1
+
+    for _ in range(steps):
+        pairs = getPairIntersection(pairs, pairInsertions)
+
+    numOfLetters = {}
+
+    for el in pairs.keys():
+        if el[0] in numOfLetters:
+            numOfLetters[el[0]] += pairs[el]
+        else:
+            numOfLetters[el[0]] = pairs[el]
+    numOfLetters[lastWord] += 1
+
+    return max(numOfLetters.values()) - min(numOfLetters.values())
+
+
+
+
 
 def getResult(dataIn, steps):
     polymerTemplate = dataIn[0]
@@ -35,5 +86,6 @@ def getResult(dataIn, steps):
 
 
 if __name__ == '__main__':
-    dataIn = open('data.txt').read().split('\n')
-    print(getResult(dataIn, 10))
+    file = 'test_data.txt'
+    print(getResult(open(file).read().split('\n'), 10))
+    print(getSecondResult(open(file).read().split('\n'), 40))
